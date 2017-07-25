e2a1707d3f9515e92a9b70fde1eabe86fd327b35
Apoorva Saxena <apsaxena24@gmail.com>
2017-07-25 19:36:18 +0100

    new data

--- app.js
+++ app.js
@@ -2,12 +2,47 @@
 	var app = angular.module('gemStore', []);
 
 	app.controller('StoreController', function() {
-		this.product = gem
+		this.products = gems
 	});
 
-	var gem = {
+	app.controller('PanelController', function() {
+		this.tab = 1
+
+		this.selectTab = function(setTab) {
+			this.tab = setTab
+			console.log(this.tab)
+		}
+
+		console.log(this.tab)
+		this.isSelected = function(checktab) {
+			return this.tab === checktab
+		}
+	});
+
+	var gems = [
+	{
 		name: 'Dodecahedron',
-		price: 2.95,
-		description : "-------......."
-	}
+		price: 2,
+		description : "-------.......",
+		can_purchase: true,
+		soldOut: false  
+	},
+	{
+   		name : "Diamond",
+   		price: 890.78,
+   		description: "just a diamond",
+   		can_purchase: true,
+   		soldOut: false 
+	}]
+
+
 })();
+
+
+// (function(){
+//   var gem = { name: 'Azurite', price: 2.95 };
+//   var app = angular.module('gemStore', []);
+//   app.controller('StoreController', function() {
+    
+//   });
+// })();
